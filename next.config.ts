edc73fa04f0562d068ce8dc26f38dbe0cb9cb423
FakuOrtiz <facundoagustinortiz@gmail.com>
2024-10-29 16:27:01 -0300

    add header

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
+import { fileURLToPath } from "url";
+import path from "path";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  sassOptions: {
+    includePaths: [path.join(__dirname, "src/sass")],
+    prependData: `@import "main.scss";`,
+  },
 };
 
 export default nextConfig;
---
+++ src/components/Background/index.tsx
@@ -0,0 +1,11 @@
+import styles from "./styles.module.scss";
+
+const Background = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.imageContainer} />
+    </div>
+  );
+};
+
+export default Background;
