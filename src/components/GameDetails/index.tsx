72f048ab265c2029a83c05de94bb107a04ead7f2
FakuOrtiz <facundoagustinortiz@gmail.com>
2024-11-01 19:49:25 -0300

    remove dynamic og, not works because of the client side

--- src/components/GameDetails/index.tsx
+++ src/components/GameDetails/index.tsx
@@ -16,65 +16,34 @@ const GameDetails = ({ id }: { id: string }) => {
 
   if (!game || loading) return <Loader />;
 
-  let imageUrl = "https://aerolab.vercel.app/default-cover.jpg";
-
-  if (game?.cover?.image_id) {
-    imageUrl = `https://images.igdb.com/igdb/image/upload/t_cover_big/${game?.cover.image_id}.png`;
-  }
-
-  const ogImageUrl = `https://aerolab.vercel.app/api/og?title=${encodeURIComponent(
-    game.name
-  )}&img=${encodeURIComponent(imageUrl)}`;
-
   return (
-    <>
-      <section className={styles.container}>
-        <GameHero
-          name={game.name}
-          publisher={game?.involved_companies?.[0]?.company?.name}
-          gameId={game.id}
-          cover={game?.cover}
-          release={game.first_release_date}
-        />
-
-        <GameStats
-          rating={game.rating}
-          release={game.first_release_date}
-          genre={game?.genres?.map((genre) => genre?.name).join(", ")}
-        />
-
-        <GameInfo
-          summary={game.summary}
-          platforms={game?.platforms
-            ?.map((platform) => platform?.name)
-            .join(", ")}
-          gallery={game?.artworks?.map((platform) => platform?.image_id)}
-        />
-
-        {game?.similar_games && game?.similar_games.length > 0 && (
-          <SimilarGames similarGames={game?.similar_games} />
-        )}
-      </section>
-
-      {/* Dynamic open graph */}
-
-      <title>{game.name}</title>
-      <meta property="og:title" content={game.name} />
-      <meta property="og:description" content={game.summary || "No summary"} />
-      <meta property="og:image" content={ogImageUrl} />
-      <meta
-        property="og:url"
-        content={`https://aerolab.vercel.app/game/${game.id}`}
+    <section className={styles.container}>
+      <GameHero
+        name={game.name}
+        publisher={game?.involved_companies?.[0]?.company?.name}
+        gameId={game.id}
+        cover={game?.cover}
+        release={game.first_release_date}
       />
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:title" content={game.name} />
-      <meta name="twitter:description" content={game.summary || "No summary"} />
-      <meta name="twitter:image" content={ogImageUrl} />
-      <meta
-        name="twitter:url"
-        content={`https://aerolab.vercel.app/game/${game.id}`}
+
+      <GameStats
+        rating={game.rating}
+        release={game.first_release_date}
+        genre={game?.genres?.map((genre) => genre?.name).join(", ")}
       />
-    </>
+
+      <GameInfo
+        summary={game.summary}
+        platforms={game?.platforms
+          ?.map((platform) => platform?.name)
+          .join(", ")}
+        gallery={game?.artworks?.map((platform) => platform?.image_id)}
+      />
+
+      {game?.similar_games && game?.similar_games.length > 0 && (
+        <SimilarGames similarGames={game?.similar_games} />
+      )}
+    </section>
   );
 };
 
