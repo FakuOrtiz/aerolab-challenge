053373dd3065472af20b18b32673bccd872da629
FakuOrtiz <facundoagustinortiz@gmail.com>
2024-11-01 19:40:26 -0300

    move dynamic metadata to game details component

--- src/components/GameDetails/index.tsx
+++ src/components/GameDetails/index.tsx
@@ -8,8 +8,6 @@ import NotFoundGame from "./components/NotFoundGame";
 import SimilarGames from "./components/SimilarGames";
 import useGameDetails from "./hooks/useGameDetails";
 import styles from "./styles.module.scss";
-import { searchGames } from "@/services/gameService";
-import { Game } from "@/models/game.model";
 
 const GameDetails = ({ id }: { id: string }) => {
   const { game, loading, error } = useGameDetails(id);
@@ -17,48 +15,6 @@ const GameDetails = ({ id }: { id: string }) => {
   if (error) return <NotFoundGame />;
 
   if (!game || loading) return <Loader />;
-
-  return (
-    <section className={styles.container}>
-      <GameHero
-        name={game.name}
-        publisher={game?.involved_companies?.[0]?.company?.name}
-        gameId={game.id}
-        cover={game?.cover}
-        release={game.first_release_date}
-      />
-
-      <GameStats
-        rating={game.rating}
-        release={game.first_release_date}
-        genre={game?.genres?.map((genre) => genre?.name).join(", ")}
-      />
-
-      <GameInfo
-        summary={game.summary}
-        platforms={game?.platforms
-          ?.map((platform) => platform?.name)
-          .join(", ")}
-        gallery={game?.artworks?.map((platform) => platform?.image_id)}
-      />
-
-      {game?.similar_games && game?.similar_games.length > 0 && (
-        <SimilarGames similarGames={game?.similar_games} />
-      )}
-    </section>
-  );
-};
-
-export default GameDetails;
-
-export async function generateMetadata({ params }: { params: { id: string } }) {
-  const { id } = params;
-
-  let game: Game | Game[] = await searchGames<Game[]>({ type: "DETAILS", id });
-
-  if (!game.length) return;
-
-  game = game[0];
 
   let imageUrl = "https://aerolab.vercel.app/default-cover.jpg";
 
@@ -70,27 +26,46 @@ export async function generateMetadata({ params }: { params: { id: string } }) {
     game.name
   )}&img=${encodeURIComponent(imageUrl)}`;
 
-  return {
-    title: game.name,
-    description: game.summary,
-    openGraph: {
-      title: game.name,
-      description: game.summary,
-      images: [
-        {
-          url: ogImageUrl,
-          width: 1200,
-          height: 630,
-          alt: game.name,
-        },
-      ],
-      url: `http://tu-dominio.com/juegos/${game.id}`,
-    },
-    twitter: {
-      card: "summary_large_card",
-      title: game.name,
-      description: game.summary,
-      images: [ogImageUrl],
-    },
-  };
-}
+  return (
+    <>
+      <title>{game.name}</title>
+      <meta property="og:title" content={game.name} />
+      <meta property="og:description" content={game.summary || "No summary"} />
+      <meta property="og:image" content={ogImageUrl} />
+      <meta
+        property="og:url"
+        content={`https://aerolab.vercel.app/game/${game.id}`}
+      />
+
+      <section className={styles.container}>
+        <GameHero
+          name={game.name}
+          publisher={game?.involved_companies?.[0]?.company?.name}
+          gameId={game.id}
+          cover={game?.cover}
+          release={game.first_release_date}
+        />
+
+        <GameStats
+          rating={game.rating}
+          release={game.first_release_date}
+          genre={game?.genres?.map((genre) => genre?.name).join(", ")}
+        />
+
+        <GameInfo
+          summary={game.summary}
+          platforms={game?.platforms
+            ?.map((platform) => platform?.name)
+            .join(", ")}
+          gallery={game?.artworks?.map((platform) => platform?.image_id)}
+        />
+
+        {game?.similar_games && game?.similar_games.length > 0 && (
+          <SimilarGames similarGames={game?.similar_games} />
+        )}
+      </section>
+    </>
+  );
+};
+
+export default GameDetails;
